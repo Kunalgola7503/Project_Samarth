939c0a23aaed35a0b2b756afc6b79ab099fe88cc
Kunal Gola <Kunalgola7703@gmail.com>
2025-10-30 17:30:14 +0530

    Configure Flask app to run on specified port
    Updated Flask app to use environment variable for port.

--- app_flask.py
+++ app_flask.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+import os
+
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend JS to communicate with backend
@@ -143,5 +145,7 @@ def ask():
 # ---------------------------
 # 6️⃣ Run Flask App
 # ---------------------------
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
